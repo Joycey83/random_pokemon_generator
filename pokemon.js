a2220d614c018958830fb60fb28dd5695583bce3
Joyce <joycecheung8385@gmail.com>
2023-09-16 13:19:15 +0100

    Added my if else statement to check if there is more than one pokemon type as some are for example electric only but some are electric and posion etc

--- pokemon.js
+++ pokemon.js
@@ -3,33 +3,18 @@ const pokemonName = document.querySelector(".pokemon-name");
 const generateBtn = document.querySelector(".generate-btn");
 const pokemonStats = document.querySelector(".stats-info");
 
-// Arrays of pokemon characters
-let pokemon = [
-  "Dedenne",
-  "Cinccino",
-  "Charizard",
-  "Snivy",
-  "Pikachu",
-  "Bulbasaur",
-];
-
-function generatePokemon() {
-  const randPokemon = pokemon[Math.floor(Math.random() * pokemon.length)];
-  console.log(randPokemon);
-}
-
 // Arrays of objects that hold the pokemon characters information
 
 let pokemonChar = [
   {
     name: "Dedenne",
-    img: "",
+    img: "./resources/images/Dedenne.png",
     species: "Antenna Pokémon",
     type: ["electric", "fairy"],
   },
   {
     name: "Cinccino",
-    img: "",
+    img: "./resources/images/Cinccino.jpg",
     species: "Scarf Pokémon",
     type: "normal",
   },
@@ -58,3 +43,28 @@ let pokemonChar = [
     species: ["grass", "posion"],
   },
 ];
+
+function generatePokemon() {
+  const randPokemonIndex = Math.floor(Math.random() * pokemonChar.length);
+  const randPokemon = pokemonChar[randPokemonIndex];
+
+  // Update DOM elements with Pokémon information
+  pokemonName.textContent = randPokemon.name;
+  document.querySelector(".pokemon-img").src = randPokemon.img; // Set the image source
+  document.querySelector(
+    ".pokemon-species"
+  ).textContent = `Species: ${randPokemon.species}`;
+
+  // Handle cases where  there is more than one pokemon'type' 
+  if (Array.isArray(randPokemon.type)) {
+    document.querySelector(
+      ".pokemon-type"
+    ).textContent = `Types: ${randPokemon.type.join(", ")}`;
+  } else {
+    document.querySelector(
+      ".pokemon-type"
+    ).textContent = `Type: ${randPokemon.type}`;
+  }
+}
+
+generatePokemon();
