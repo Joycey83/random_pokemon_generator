6fe68a31db851bf34820a90b575c80b07b18ace4
Joyce <Jcheung8385@gmail.com>
2023-09-16 18:20:23 +0100

    Trying to separate the two type array by indexes

--- pokemon.js
+++ pokemon.js
@@ -133,7 +133,7 @@ function generatePokemon() {
   if (Array.isArray(randPokemon.type)) {
     document.querySelector(
       ".pokemon-type"
-    ).textContent = `Types: ${randPokemon.type.join(", ")}`;
+    ).textContent = `Types: ${randPokemon.type[0]}, ${randPokemon.type[1]}`;
   } else {
     document.querySelector(
       ".pokemon-type"
