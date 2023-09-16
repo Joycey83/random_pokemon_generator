c5ba500e7482651a1ba2d35c1172d373cd5effa7
Joyce <joycecheung8385@gmail.com>
2023-09-16 12:11:28 +0100

    created an array of pokemon objects

---
+++ pokemon.js
@@ -0,0 +1,40 @@
+// Arrays of objects that hold the pokemon characters and information
+
+let pokemonChar = [
+  {
+    name: "Dedenne",
+    img: "",
+    species: "Antenna Pokémon",
+    type: ["electric", "fairy"],
+  },
+  {
+    name: "Cinccino",
+    img: "",
+    species: "Scarf Pokémon",
+    type: "normal",
+  },
+  {
+    name: "Charizard",
+    img: "",
+    species: "",
+    type: "",
+  },
+  {
+    name: "Snivy",
+    img: "",
+    species: "Grass Snake Pokémon",
+    type: "grass",
+  },
+  {
+    name: "Pikachu",
+    img: "",
+    species: "Mouse Pokémon",
+    type: "electric",
+  },
+  {
+    name: "Bulbasaur",
+    img: "",
+    type: "Seed Pokémon",
+    species: ["grass", "posion"],
+  },
+];
