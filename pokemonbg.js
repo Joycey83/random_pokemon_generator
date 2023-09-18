e029fd930380bf74be0a763f30ee65b8e2246939
Joyce <Jcheung8385@gmail.com>
2023-09-18 18:08:00 +0100

    created an background image array

---
+++ pokemonbg.js
@@ -0,0 +1,9 @@
+// Create function with set interval so the images will randomly change
+
+let backgroundImg = [
+  "./resources/images/pokeball.jpg",
+  "./resources/images/pokemon-group.jpg",
+  "./resources/images/evil-pokemon.jpg",
+];
+
+function showChangeImg() {}
