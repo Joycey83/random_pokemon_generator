27e0adc6c19973f2c9c0542dc270adc4f6b59986
Joyce <Jcheung8385@gmail.com>
2023-09-18 18:24:33 +0100

    Created an showImage function for the random pokemon background images

--- pokemonbg.js
+++ pokemonbg.js
@@ -1,9 +1,18 @@
 // Create function with set interval so the images will randomly change
 
-let backgroundImg = [
+let bgImg = [
   "./resources/images/pokeball.jpg",
   "./resources/images/pokemon-group.jpg",
   "./resources/images/evil-pokemon.jpg",
 ];
 
-function showChangeImg() {}
+const backgroundImg = document.querySelector("bg-image"); // Access the DOM body element
+
+function showChangeImg() {
+  const bgImgRandoIndex = Math.floor(Math.random() * bgImg.length); // random images choosen
+  //   Access the random pokemon images by index
+  const randoImages = bgImg[bgImgRandoIndex];
+//   setting the random background images
+  backgroundImg.style.backgroundImage = randoImages;
+}
+setInterval(showChangeImg, 2000); // time delay for each images
