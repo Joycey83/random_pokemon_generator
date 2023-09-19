072937881b9c112c3852faef8a228192ab32d55a
Joyce <Jcheung8385@gmail.com>
2023-09-19 17:42:48 +0100

    Added a pokemon image for loading for the window loading delay time

--- pokemon2.js
+++ pokemon2.js
@@ -17,4 +17,4 @@ function showChangeImg() {
   //   setting the random background images
   backgroundImg.style.backgroundImage = `${gradientOverlay}, url(' ${randoImage}')`;
 }
-setInterval(showChangeImg, 1500); // time delay for each images
+setInterval(showChangeImg, 2300); // time delay for each images
