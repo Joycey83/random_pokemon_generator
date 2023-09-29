d015f7ccf75c57be782236a56abab95a963c3d01
Joyce <Jcheung8385@gmail.com>
2023-09-29 12:28:15 +0100

    created a statspan function, created a foreach loop to loop through each pokemon and then add the span to it

--- resources/js/pokemon.js
+++ resources/js/pokemon.js
@@ -91,4 +91,24 @@ function createColorSpan(type) {
   }
 }
 
+// Function to create a span for a stat
+function createStatSpan(statName, statValue) {
+  const statSpan = document.createElement("span");
+  statSpan.textContent = `${statName}: ${statValue}`;
+  return statSpan;
+}
+
+// Loop through each Pokemon and add stat spans
+pokemonChar.forEach((pokemon) => {
+  const statContainer = document.createElement("div");
+
+  for (const stat in pokemon.stats) {
+    const statValue = pokemon.stats[stat];
+    const statSpan = createStatSpan(stat, statValue);
+    statContainer.appendChild(statSpan);
+  }
+
+  pokemon.stats.spans = statContainer.innerHTML;
+});
+
 generateBtn.addEventListener("click", generatePokemon);
