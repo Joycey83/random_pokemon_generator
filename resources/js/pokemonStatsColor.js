0da3e83edee8aab02cee7f071737e5831b99bac5
Joyce <Jcheung8385@gmail.com>
2023-10-08 15:59:21 +0100

    Deleted hex color references and created a object with CSS class names to be used to map different color stat spans

---
+++ resources/js/pokemonStatsColor.js
@@ -0,0 +1,10 @@
+// Defined a mapping of stat color level to CSS classes
+
+const statsColor = {
+  "drk.green": "#00C2B8",
+  "mid.green": "#23CD5E",
+  "lgt.green": "#A0E515",
+  yellow: "#FFDD57",
+  orange: "#FF7F0F",
+  red: "#F34444",
+};
