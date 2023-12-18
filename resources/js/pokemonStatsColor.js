616496c98fa8b12a5f6b94a8216e169ba183d9a1
Joyce <Jcheung8385@gmail.com>
2023-12-18 12:21:26 +0000

    created a colorTypespan function so that the pokemon stats will be color coded

--- resources/js/pokemonStatsColor.js
+++ resources/js/pokemonStatsColor.js
@@ -1,10 +1,24 @@
 // Defined a mapping of stat color level to CSS classes
 
 const statsColor = {
-  "drk.green": "#00C2B8",
-  "mid.green": "#23CD5E",
-  "lgt.green": "#A0E515",
+  "drk-green": "#00C2B8",
+  "mid-green": "#23CD5E",
+  "lgt-green": "#A0E515",
   yellow: "#FFDD57",
   orange: "#FF7F0F",
   red: "#F34444",
 };
+
+// Create a function to get the CSS class for a given type
+function getStatsColor(type) {
+  return statsColor[type.loLowerCase()] || "pokemon-type-span"; // default style for the span
+}
+
+// Usage in your code
+function createColorSpan(type) {
+  const ColorTypeSpan = document.createElement("color-span");
+  ColorTypeSpan.innerHTML = type;
+  ColorTypeSpan.classList.add("pokemon-type-span", getStatsColor(type));
+  statsInfoContainer.appendChild(ColorTypeSpan);
+  return ColorTypeSpan;
+}
