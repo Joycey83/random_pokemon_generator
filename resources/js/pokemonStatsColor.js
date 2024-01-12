1499e4328f5280b6eb7e1ba6468c1d191b11894e
Joyce <Jcheung8385@gmail.com>
2024-01-12 16:09:22 +0000

    trying to fix this bug/error

--- resources/js/pokemonStatsColor.js
+++ resources/js/pokemonStatsColor.js
@@ -9,16 +9,16 @@ function createStatColorSpan(statType, statValue) {
   };
 
   const defaultColor = "pokemon-type-span";
-  const backgroundColor = statColors[statType.toLowerCase()] || defaultColor;
+  let backgroundColor = statColors[statType.toLowerCase()] || defaultColor;
 
   // Set background color based on the condition
   if (statValue > 65) {
-    return `${backgroundColor} greater-than-65`;
+    backgroundColor += " greater-than-65";
   } else if (statValue < 65) {
-    return `${backgroundColor} lower-than-65`;
-  } else {
-    return backgroundColor;
+    backgroundColor += " lower-than-65";
   }
+
+  return backgroundColor;
 }
 
 export default createStatColorSpan;
